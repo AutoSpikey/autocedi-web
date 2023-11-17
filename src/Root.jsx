eaf3a6dddebf84f264bfcd2ee44af93a98eb9f49
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2023-11-17 16:53:58 +0000

    nav and layout component

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,12 +1,14 @@
-import { NavLink, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import NavItem from './components/NavItem'
 
 export default function Root() {
   return (
-    <div className='v-full'>
-        <div id="navbar" className='flex'>
-            <NavLink to="/">Autocedi</NavLink>
-            <NavLink to="automations/create">Automations</NavLink>
-        </div>
+    <div className='v-full align-middle'>
+      <div className="m-2 p-2 border-b ">
+        <NavItem to="/" label="Home" />
+        <NavItem to="automations/create" label="Automations" />
+      </div>
+
         <div id="page" className='h-full w-full'>
             <Outlet />
         </div>
---
+++ src/components/NavItem.jsx
@@ -0,0 +1,15 @@
+import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
+
+export default function NavItem({ to, label }) {
+	return (
+		<NavLink className="m-2 p-2 hover:bg-slate-100 " to={to}>
+			{label}
+		</NavLink>
+	);
+}
+
+NavItem.prototypes = {
+	label: PropTypes.string.isRequired,
+	to: PropTypes.string.isRequired,
+};
